1f4148e0f98d1aecac97ed6ab83a1bb573ce3cdf
Etienne ROBERT <etrobert@e3r10p21.42.fr>
2017-01-26 21:20:06 +0100

    Norme correction

--- srcs/ft_set/ft_set_e_new.c
+++ srcs/ft_set/ft_set_e_new.c
@@ -6,7 +6,7 @@
 /*   By: etrobert <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/20 19:41:53 by etrobert          #+#    #+#             */
-/*   Updated: 2017/01/20 19:43:44 by etrobert         ###   ########.fr       */
+/*   Updated: 2017/01/26 21:19:37 by etrobert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ t_set_e			*ft_set_e_new(void *val, t_set_e *right, t_set_e *left,
 		t_set_e *parent)
 {
 	t_set_e		*elem;
-	
+
 	if ((elem = (t_set_e *)(malloc(sizeof(t_set_e)))) == NULL)
 		return (NULL);
 	elem->val = val;
