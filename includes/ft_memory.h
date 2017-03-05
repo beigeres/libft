53a652330d1a1e15172ff47e88ab9127947391e4
Etienne ROBERT <etrobert@e3r10p22.42.fr>
2017-03-05 22:23:07 +0100

    memory structure

--- includes/ft_memory.h
+++ includes/ft_memory.h
@@ -6,7 +6,7 @@
 /*   By: etrobert <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/01 18:02:35 by etrobert          #+#    #+#             */
-/*   Updated: 2017/01/16 11:39:51 by etrobert         ###   ########.fr       */
+/*   Updated: 2017/03/05 16:02:23 by etrobert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,20 +19,26 @@
 
 # include <stdlib.h>
 
-void	*ft_memalloc(size_t size);
-void	ft_memdel(void **ap);
+typedef struct	s_memory
+{
+	void			*ptr;
+	size_t			size;
+}				t_memory;
 
-void	*ft_memset(void *s, int c, size_t n);
-void	ft_bzero(void *s, size_t n);
-void	*ft_memcpy(void *dest, const void *src, size_t n);
-void	*ft_memccpy(void *dest, const void *src, int c, size_t n);
-void	*ft_memmove(void *dest, const void *src, size_t n);
-void	*ft_memchr(const void *s, int c, size_t n);
-int		ft_memcmp(const void *s1, const void *s2, size_t n);
+void			*ft_memalloc(size_t size);
+void			ft_memdel(void **ap);
 
-void	*ft_memdup(const void *ptr, size_t size);
-void	*ft_nrealloc(void *ptr, size_t old_size, size_t new_size);
+void			*ft_memset(void *s, int c, size_t n);
+void			ft_bzero(void *s, size_t n);
+void			*ft_memcpy(void *dest, const void *src, size_t n);
+void			*ft_memccpy(void *dest, const void *src, int c, size_t n);
+void			*ft_memmove(void *dest, const void *src, size_t n);
+void			*ft_memchr(const void *s, int c, size_t n);
+int				ft_memcmp(const void *s1, const void *s2, size_t n);
 
-void	ft_swap_int(int *a, int *b);
+void			*ft_memdup(const void *ptr, size_t size);
+void			*ft_nrealloc(void *ptr, size_t old_size, size_t new_size);
+
+void			ft_swap_int(int *a, int *b);
 
 #endif
