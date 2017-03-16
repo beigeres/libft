1f40b30476f72f1deb76978263593cf404d04487
Thomas BELDAME <tbeldame@e2r10p12.42.fr>
2017-03-16 19:19:34 +0100

    Fix includes and norm

--- srcs/ft_string/ft_isnumber.c
+++ srcs/ft_string/ft_isnumber.c
@@ -6,12 +6,11 @@
 /*   By: tbeldame <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/26 11:28:17 by tbeldame          #+#    #+#             */
-/*   Updated: 2017/03/16 19:04:14 by tbeldame         ###   ########.fr       */
+/*   Updated: 2017/03/16 19:18:50 by tbeldame         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
-#include <stdbool.h>
+#include "ft_string.h"
 
 bool	ft_isnumber(char *str)
 {
@@ -19,7 +18,7 @@ bool	ft_isnumber(char *str)
 		return (false);
 	if (*str == '-' || *str == '+')
 		++str;
-	if (!ft_str_test_chars(str, &ft_isdigit)
-			return (false);
+	if (!ft_str_test_chars(str, &ft_isdigit))
+		return (false);
 	return (true);
 }
