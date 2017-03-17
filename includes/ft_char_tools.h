e21fd37131b12e90d18b32aa28a912cc7bb04e65
Maxime VERDIER <mverdier@e2r7p1.42.fr>
2017-03-17 16:47:20 +0100

    add ft_isnumber

--- includes/ft_char_tools.h
+++ includes/ft_char_tools.h
@@ -6,7 +6,7 @@
 /*   By: etrobert <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/28 19:11:30 by etrobert          #+#    #+#             */
-/*   Updated: 2017/03/06 17:57:11 by etrobert         ###   ########.fr       */
+/*   Updated: 2017/03/17 16:46:53 by mverdier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,7 @@ bool			ft_isascii(char c);
 bool			ft_isprint(char c);
 bool			ft_isblank(char c);
 bool			ft_isspace(char c);
+bool			ft_isnumber(char *str);
 
 char			ft_toupper(char c);
 char			ft_tolower(char c);
--- includes/ft_string.h
+++ includes/ft_string.h
@@ -6,7 +6,7 @@
 /*   By: etrobert <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/01 17:19:59 by etrobert          #+#    #+#             */
-/*   Updated: 2017/03/16 19:10:06 by tbeldame         ###   ########.fr       */
+/*   Updated: 2017/03/17 16:46:54 by mverdier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -66,6 +66,5 @@ char			**ft_strsplit_str(const char *s, char *chars);
 bool			ft_str_test_chars(char *str, bool (*test)(char));
 int				ft_atoi(const char *str);
 char			*ft_itoa(int n);
-bool			ft_isnumber(char *str);
 
 #endif
